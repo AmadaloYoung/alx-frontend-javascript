e33ca38762714e3153d3a997715f05e0783a0ea0
Giggs Bradly <giggsb9@gmail.com>
2023-01-31 00:04:02 +0300

    Using filter and map combined

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => (student.city === city))
     .map((student) => {
-      const grade = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = newGrades.filter((grade) => grade.studentId === student.id);
       return { ...student, grade: grade ? grade.grade : 'N/A' };
     });
 }
